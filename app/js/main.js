9f1b87b7bb7fc5e9281a780822ac4f3f152672c8
crazypushok0 <crazypushok0@gmail.com>
2019-12-20 18:36:51 +0200

    creat page product-one and popup

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,14 @@ $(function(){
   $('.header__btn-menu').on('click', function(){
     $('.header__box-btn').toggleClass('active');
   });
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
 
 var mixer = mixitup('.products__inner-box');
 
