7d0c98b80134380fef1fda2924771d563dc8bd8f
crazypushok0 <crazypushok0@gmail.com>
2019-12-21 21:21:26 +0200

    create settings page

--- app/js/main.js
+++ app/js/main.js
@@ -46,6 +46,14 @@ $(function(){
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
     });
+    $('.settings__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
 var mixer = mixitup('.products__inner-box');
 
