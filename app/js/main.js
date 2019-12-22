c1ab56e3fa54c86fcc3dc760018b328597d82f8a
crazypushok0 <crazypushok0@gmail.com>
2019-12-22 21:04:51 +0200

    create profile-author page

--- app/js/main.js
+++ app/js/main.js
@@ -46,14 +46,22 @@ $(function(){
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
     });
-    $('.settings__tabs .tab').on('click', function(event) {
+  $('.settings__tabs .tab').on('click', function(event) {
+     var id = $(this).attr('data-id');
+       $('.settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+       $('.settings__tabs .tabs').find('.tab').removeClass('active');
+       $(this).addClass('active');
+       $('#'+id).addClass('active-tab').fadeIn();
+       return false;
+    });
+  $('.profile__tabs .tab').on('click', function(event) {
       var id = $(this).attr('data-id');
-        $('.settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.settings__tabs .tabs').find('.tab').removeClass('active');
-        $(this).addClass('active');
-        $('#'+id).addClass('active-tab').fadeIn();
-        return false;
-      });
+      $('.profile__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.profile__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });    
 
 var mixer = mixitup('.products__inner-box');
 
